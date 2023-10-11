64af601b7a191470dd34bef7595e346dcadb6d84
Grisha <36422717+webdevgrisha@users.noreply.github.com>
2023-10-11 22:20:26 +0200

    First way: using algebraic solution

--- src/index.js
+++ src/index.js
@@ -1,71 +1,64 @@
-module.exports = function toReadable(number) {
-    const numbers = {
-        0: "zero",
-        1: "one",
-        2: "two",
-        3: "three",
-        4: "four",
-        5: "five",
-        6: "six",
-        7: "seven",
-        8: "eight",
-        9: "nine",
-        10: "ten",
-        11: "eleven",
-        12: "twelve",
-        13: "thirteen",
-        20: "twenty",
-        30: "thity",
-        40: "forty",
-        50: "fifty",
-        60: "sixty",
-        70: "seventy",
-        80: "eighty",
-        90: "ninety",
-        100: "hondred",
-        1000: "thousand",
-    };
+const numbers = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+};
 
-    let result;
+module.exports = function toReadable(num) {
+  const resultArr = [];
+  let currentNum = num;
 
-    if (number < 14) {
-        return numbers[String(number)];
-    } else if (number >= 14 && number < 20) {
-        result = numbers[String(number % 10)];
-        result = result.at(-1) === "e" ? result.slice(0, -1) : result;
+  if(num in numbers) return numbers[num];
 
-        return result + "teen";
-    } else if (number >= 20 && number < 100) {
-        let remainder = number % 10;
+  for(let iterationNum = 0; currentNum !== 0; iterationNum++) {
+    let numStringRep = '';
+    const reminder = currentNum % 10;
 
-        if (remainder === 0) {
-            result = numbers[String(number)];
-        } else {
-            let decimal = numbers[String(Math.trunc((number / 10) * 10))];
+    currentNum = Math.floor(currentNum / 10);
 
-            result = decimal + " " + numbers[String(remainder)];
-        }
-    } else if (number >= 100 && number < 1000) {
-        let unitsRemainder = number % 10;
-        let hundredRemainder = number % 100;
-        let hundredCount = Math.trunc(number / 100);
+    if(reminder === 0) continue;
 
-        if (hundredRemainder === 0) {
-            result = numbers[String(hundredCount)] + " " + "hundred";
-        } else {
-            let decimal = numbers[String(Math.trunc((number / 10) * 10))];
+    const clearNum = reminder * (10 ** iterationNum);
+    
 
-            console.log(hundredCount);
-            result =
-                numbers[String(hundredCount)] +
-                " " +
-                "hundred" +
-                " " +
-                decimal +
-                " " +
-                numbers[String(unitsRemainder)];
-        }
+    if(iterationNum === 2) {
+      numStringRep = `${numbers[reminder]} hundred`;
+    } else {
+      numStringRep = numbers[clearNum];
     }
 
-    return result;
+    resultArr.push(numStringRep);
+  }
+
+  if(num > 100) {
+    let reminder = num % 100;
+    reminder >= 11 && reminder < 20 ? resultArr.splice(0, 2, numbers[reminder]) : null;
+  }
+
+  return resultArr.reverse().join(' ').trim();
 };
